bb52d901266ff58a5e53345b38e0a0d9295c114f
Aishah <aishah122005@gmail.com>
2023-11-29 10:04:16 -0600

    created initial view

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -13,9 +13,11 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Tic Tac Toe")
         }
-        .padding()
+        .preferredColorScheme(.dark)
+        
+        
     }
 }
 
